120d21567c7e7bd384847e5211270f9e6376640b
Emmanuel Ezeani <monaoffice20@gmail.com>
2023-02-25 19:30:34 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,102 @@
-import React from 'react'
-import Homepage from './pages/Homepage'
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { auth_routes_group } from "./routes/auth";
+import { dashboard_route_group } from "./routes/dashboard";
+import React, { useEffect } from "react";
+import store from "./redux/store";
+import { Provider, useDispatch } from "react-redux";
+import setAuthToken from "./utils/auth";
+import PrivateRoute from "./components/auth/PrivateRoute";
+// import { getUser } from "./redux/user";
+// import { ToastContainer } from "react-toastify";
+import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
+import Homepage from "./pages/Homepage";
 
-const App = () => {
+function App() {
+  const location = useLocation();
+  setAuthToken();
+
+  // useEffect(() => {
+
+  // }, [])
+
+  //initialize electricity bill on time confirmation
+
   return (
-    <div>
-      <Homepage />
-    </div>
-  )
+    <Provider store={store}>
+      {/* <ElectricityOntime /> */}
+      <div className="App">
+        {/* <Helmet>
+          <title>Aprokopay || Sell & Buy Informations </title>
+          <meta
+            name="description"
+            content="Sell and buy informations."
+          />
+          <meta
+            property="og:description"
+            content="Aprokopay allows you sell and buy informations."
+          />
+          <meta
+            name="keywords"
+            content="marketplace, buy, sell"
+          />
+          <meta name="page-topic" content="eCommerce" />
+          <meta name="page-type" content="Landing" />
+          <meta name="audience" content="Everyone" />
+          <meta
+            property="og:image"
+            content="https://pbs.twimg.com/profile_images/1485498638973517826/aX6F2jHy_400x400.jpg"
+          />
+          <meta property="og:url" content="https://getravenbank.com" />
+          <meta name="robots" content="index, follow" />
+          <meta
+            property="twitter:image:src"
+            content="https://pbs.twimg.com/profile_images/1485498638973517826/aX6F2jHy_400x400.jpg"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image:alt" content="GetRavenBank" />
+          <meta
+            name="twitter:site"
+            content="https://www.twitter.com/theravenbank"
+          />
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;"
+          />
+        </Helmet> */}
+          <Routes location={location}>
+            {/* riderect to login on load start */}
+            <Route
+              location={location}
+              path="/"
+              element={<Homepage />}
+            ></Route>
+            <Route
+              location={location}
+              path="*"
+              element={<Navigate to="/" />}
+            ></Route>
+
+            {/*  */}
+            {/* auth route group start */}
+            {auth_routes_group.map((route, idx) => {
+              return <Route location={location} key={idx} exact {...route} />;
+            })}
+            {/* auth route group end */}
+            {/* auth route group start */}
+            {dashboard_route_group.map((route, idx) => {
+              return (
+                <Route element={<PrivateRoute />} key={idx}>
+                  <Route location={location} key={idx} exact {...route} />;
+                </Route>
+              );
+            })}
+            {/* auth route group end */}
+          </Routes>
+      </div>
+      <Toaster />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
