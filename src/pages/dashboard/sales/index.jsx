302d0242fae00e315dab9bfcf2cd5ba5ca9d1c90
Emmanuel Ezeani <monaoffice20@gmail.com>
2023-04-08 09:28:30 +0100

    admin dashboard 90% done

--- src/pages/dashboard/sales/index.jsx
+++ src/pages/dashboard/sales/index.jsx
@@ -4,7 +4,85 @@ import DashboardLayout from '../../../layouts/DashboardLayout'
 function Sales() {
   return (
     <DashboardLayout>
-    <div>Sales</div>
+    <div className="sales_wrapper">
+      <div className="page_top">
+        <span className="page_title">
+          <h6> Sales Portal</h6>
+          <p>Your sales portal gives you a bried overview of your dales and order statistics</p>
+        </span>
+
+        <div className="stats_card">
+          <div className="stat">
+            <span>
+              <p>Total Income</p>
+              <h6>{formatNumWithCommaNaira('5000')}</h6>
+            </span>
+            <figure>
+              {icons.chart}
+            </figure>
+          </div>
+
+          <div className="stat">
+            <span>
+              <p>Total Income</p>
+              <h6>{formatNumWithCommaNaira('5000')}</h6>
+            </span>
+            <figure>
+              {icons.chart}
+            </figure>
+          </div>
+
+          <div className="stat_minimal">
+            <span>
+              <p>Informations Sold:</p>
+              <h6>{`23`}</h6>
+            </span>
+            <span>
+              <p>Informations Bought:</p>
+              <h6>{`10`}</h6>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="page_body">
+                   {/* table start */}
+                   <div className="table-wrap">
+              <RavenTable headerList={headerList} action>
+                {bodyList.map((chi, idx) => {
+                  const { amount, date, direction, narration, status, type } =
+                    chi;
+                  return (
+                    <RavenTableRow
+                      onEdit={() => {
+                        setShowModal(true);
+                      }}
+                      action
+                      key={idx}
+                      one={rowTypeText(direction, narration)}
+                      two={amount}
+                      three={batchTypeWrap(type)}
+                      four={date}
+                      five={formatTypeWrap(status)}
+                    />
+                  );
+                })}
+                
+              </RavenTable>
+              {/* pagination start */}
+              <div className="table-pagination-box">
+                <RavenPagination
+                  color={`black-light`}
+                  blackHover
+                  currentPage={1}
+                  totalPage={7}
+                />
+              </div>
+              {/* pagination end */}
+            </div>
+            {/* table end */}
+      </div>
+    </div>
 
     </DashboardLayout>
   )
