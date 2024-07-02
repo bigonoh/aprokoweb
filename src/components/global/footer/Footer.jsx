0141781756f949d4fc7e9c43d3c7eea787f8a0c2
granezer <onohkmichael@gmail.com>
2024-07-02 13:46:33 -0700

    commit

--- src/components/global/footer/Footer.jsx
+++ src/components/global/footer/Footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
         <div className="flex flex-column ">{icons.logo_white}</div>
         <div className="flex content-items flex-column gap-10">
           <p className="text-xsm footer-header text-700">Social</p>
-          <NavLink to="https://twitter.com/aprokopay"><p className="text-md item pt-10">Twitter</p></NavLink>
-          <NavLink to="https://www.instagram.com/therealaprokopay?igsh=Y216OWRhZmRiNm9v"><p className="text-md item">Instagram</p></NavLink>
+          <link to="https://twitter.com/aprokopay"><p className="text-md item pt-10">Twitter</p></link>
+          <link to="https://www.instagram.com/therealaprokopay?igsh=Y216OWRhZmRiNm9v"><p className="text-md item">Instagram</p></link>
           <NavLink to="https://www.linkedin.com/company/aprokopay/?lipi=urn%3Ali%3Apage%3Aorganization_admin_admin_feed_index%3Be197d0eb-ced8-4736-a25e-c2a42445a30c"><p className="text-md item">Linkedin</p></NavLink>
           <NavLink to="https://wa.me/qr/ILKFMRF4ZAEAA1"><p className="text-md item">Whatsapp</p></NavLink>
           <NavLink to="https://www.facebook.com/profile.php?id=61558559893434&mibextid=ZbWKwL"><p className="text-md item">Facebook</p></NavLink>
